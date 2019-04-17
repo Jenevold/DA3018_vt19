f446e050b1a7973bed2abc04981943eb0581c70a
Jens Enevold <jensenevold@gmail.com>
2019-04-17 19:15:15 +0200

    more prints

--- lab4/courseinfo/CourseInfo.java
+++ lab4/courseinfo/CourseInfo.java
@@ -8,10 +8,13 @@ public class CourseInfo {
 		courses.insert("DA3018", "Computer Science", 7.5);
 		courses.insert("MM2001", "Matematik I", 30.0);
 		courses.insert("DA2004", "Programmeringsteknik", 7.5);
+		courses.insert("MM5014", "Numerisk analys", 7.5);
 		
 		int n = courses.size();
 		System.out.printf("There are %d courses in the database.\n", n);
 		
+		String test = courses.find("MM5014").getCourseName(); //test för att se output om man söker ågot som inte finns
+		System.out.printf("Name: %s\n", test); 					//denna skriver ut raden över
 		String name = courses.find("MM2001").getCourseName();
 		String code = courses.find("MM2001").getCourseCode();
 		Double credit = courses.find("MM2001").getCredits();
